12ad5624ad665abc2fbca977caae0a70b085912d
Jorge-Ceron <a01278485@tec.mx>
2025-09-22 11:20:48 -0600

    Intake
    Intake basic sketching

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.Intake;
+
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+
+public class IntakeConstants {
+  public static final int INTAKE_ID = 0;
+  public static final double GEARBOX_REDUCTION = 0;
+  public static final double MIN_OFFSET = 0;
+  public static final Angle NONE = Units.Rotations.of(0);
+}
