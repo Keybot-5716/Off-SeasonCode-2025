53ce88fe0ef0a86eabc701437063824adb9c823f
Jorge-Ceron <a01278485@tec.mx>
2025-09-24 17:33:46 -0600

    Intake Subsystem Sketch

--- src/main/java/frc/robot/subsystems/Intake/IntakeSubsys.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeSubsys.java
@@ -1,1 +1,143 @@
-// W.I.P
+package frc.robot.subsystems.elevator;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.Alert;
+import edu.wpi.first.wpilibj.Alert.AlertType;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Robot;
+import org.littletonrobotics.junction.Logger;
+
+public class IntakeSubsys extends SubsystemBase {
+
+  private final IntakeIO io;
+  private final IntakeIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+
+  private final Debouncer motorConnectedDebouncer = new Debouncer(0.5, DebounceType.kFalling);
+  private final Alert motorDisconnected =
+      new Alert("Intake Motor Disconnected! D:", AlertType.kWarning);
+
+  private Angle lastDesiredAngle = Units.Rotations.of(0);
+  private double desiredElevatorPosition;
+  private double desiredOutput;
+
+  private DesiredState desiredState = DesiredState.STOPPED;
+  private DesiredState lastDesiredState = DesiredState.STOPPED;
+  private SubsystemState subsystemState = SubsystemState.STOPPING;
+
+  public enum DesiredState {
+    STOPPED,
+    HOME,
+    DEPLOYED,
+    MANUAL
+  }
+
+  public enum SubsystemState {
+    STOPPING,
+    HOMING,
+    DEPLOYING,
+    MANUAL
+  }
+
+  public ElevatorSubsystem(ElevatorIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Intake", inputs);
+
+    motorDisconnected.set(
+        !motorConnectedDebouncer.calculate(inputs.data.motorConnected() && !Robot.isJITing()));
+
+    Logger.recordOutput("Intake/Current Position", getElevatorPosInRotations());
+    Logger.recordOutput("Intake/Desired Position", lastDesiredAngle.in(Units.Rotations));
+    Logger.recordOutput("Intake/IsAtDesiredPosition", isAtDesiredPos());
+    Logger.recordOutput("Intake/ElevatorIndividualState", lastDesiredState);
+
+    lastDesiredState = this.desiredState;
+  }
+
+  public void setPosition(double position) {
+    io.setPosition(position);
+    lastDesiredAngle = Units.Rotations.of(position);
+  }
+
+  public void runOpenLoop(double output) {
+    io.runOpenLoop(output);
+  }
+
+  public void setVoltage(Voltage voltage) {
+    io.setVoltage(voltage.in(Units.Volts));
+  }
+
+  public Angle getElevatorPosInRotations() {
+    return Units.Rotations.of(inputs.data.positionRotations());
+  }
+
+  public Angle getLastDesiredIntakePosInRotations() {
+    return lastDesiredAngle;
+  }
+
+  public void resetEncoder() {
+    io.setPosition(0);
+  }
+
+  public boolean isPositionedRotations(Angle position, Angle offset) {
+    return MathUtil.isNear(
+        position.in(Units.Rotations),
+        getIntakePosInRotations().in(Units.Rotations),
+        offset.in(Units.Rotations));
+  }
+
+  public boolean isAtDesiredPos() {
+    return isPositionedRotations(
+        getLastDesiredIntakePosInRotations(), Units.Rotations.of(IntakeConstants.MIN_OFFSET));
+  }
+
+  public SubsystemState setStateTransitions() {
+    return switch (desiredState) {
+      case HOME -> SubsystemState.HOMING;
+      case STOPPED -> SubsystemState.STOPPING;
+      case PREP_LVL -> SubsystemState.PREPARING_LVL;
+      case MANUAL -> SubsystemState.MANUAL;
+      default -> SubsystemState.STOPPING;
+    };
+  }
+
+  public void applyStates() {
+    switch (subsystemState) {
+      case HOMING:
+        io.setPosition(IntakeConstants.NONE.in(Units.Rotations));
+        break;
+      case STOPPING:
+        io.stop();
+        break;
+      case PREPARING_LVL:
+        io.setPosition(desiredIntakePosition);
+        break;
+      case MANUAL:
+        io.runOpenLoop(desiredOutput);
+        break;
+    }
+  }
+
+  public void setDesiredState(DesiredState desiredState) {
+    this.desiredState = desiredState;
+  }
+
+  public void setDesiredState(DesiredState desiredState, double desiredIntakePosition) {
+    this.desiredState = desiredState;
+    this.desiredIntakePosition = desiredIntakePosition;
+  }
+
+  public void setDesiredStateWithOutput(DesiredState desiredState, double desiredOutput) {
+    this.desiredState = desiredState;
+    this.desiredOutput = desiredOutput;
+  }
+}
